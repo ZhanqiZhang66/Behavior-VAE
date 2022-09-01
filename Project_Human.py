1d320ef1c30ffaa14415d592e6e6e6acc2496b71
ZhanqiZhang66 <31415280+ZhanqiZhang66@users.noreply.github.com>
2022-08-31 20:29:33 -0700

    add sliding window in transition m

--- Project_Human.py
+++ Project_Human.py
@@ -1,19 +1,25 @@
 # Created by Victoria Zhang at 4/24/2022
 # File: Project_Human.py
-# Description:  main function of human project
+# Description: main function of human project
 # Scenario:
 # Usage:
 
 #%%
-import pandas as pd
+import bound_n_rotate, analyze_dlc, dwell_time, itop, latent_visualization, scoring_analysis, transition_matrix
 
-#%% Flags
+#%%  Flags
 
-#%% PREPROCESS DLC markers, skeletons
-
-# Import DLC annotations, crop, save info, and align frames egocentrically
-
-#%% PREPROCESS Scorings
-
-#%%
+#%%  PREPROCESS DLC markers, skeletons
+bound_n_rotate # Import DLC annotations, crop, save info, and align frames egocentrically
+analyze_dlc
+#%%  PREPROCESS Scorings
+scoring_analysis
+#%%  RUN VAME
+itop
+#%%  dwell time (state-wise) ANALYSIS
+dwell_time
+#%% transition (between states) ANALYSIS
+transition_matrix
+#%%  latent representation (dynamics) ANALYSIS
+latent_visualization
 
--- transition_matrix.py
+++ transition_matrix.py
@@ -1,6 +1,7 @@
 # Created by Victoria Zhang at 8/15/2022
 # File: transition_matrix.py
-# Description: 
+# Description: upload transition matrix for each video, and apply L0(spasity, entropy),
+# L1/L2, and more metrics to analyze
 # Scenario:
 # Usage:
 #%%
@@ -27,7 +28,6 @@ start_frame = pd.read_csv('G:\start_frame.csv')
 
 titles = ["CP", "BD"]
 N = [0, 0]
-
 
 Motif_usages = [[], []]
 Motif_usage_pct = [[], []]
@@ -105,7 +105,7 @@ def add_self_transition(transition_m, last_state):
     len_reduced = np.sum(np.all(transition == 0, axis=1))
     n_rows_removed = np.shape(transition_m)[0] - np.shape(transition)[0]
     if len_reduced:
-        transition[last_state- n_rows_removed][last_state- n_rows_removed] = 1
+        transition[last_state - n_rows_removed][last_state - n_rows_removed] = 1
     return transition
 
 def compute_l0_entropy(transition_m, last_state):
@@ -116,13 +116,13 @@ def compute_l0_entropy(transition_m, last_state):
         stationary = np.array(U[:, np.where(np.abs(S - 1.) < 1e-8)[0][0]].flat)
         stationary = stationary / np.sum(stationary)
         stationary = stationary.real
-        H = scipy.stats.entropy(invertible_T, base=2)
+        H = scipy.stats.entropy(invertible_T, base=2, axis=1)
         entropy = stationary.dot(H)
     else:
         entropy = 0
     return entropy
 
-#%%
+#%% Load transition matrix in each video, and append into two classes (BD, CP)
 for j, videos in enumerate([control_videos, BD_videos]):
     n = 0
     for i in range(len(videos)):
@@ -154,9 +154,9 @@ for j, videos in enumerate([control_videos, BD_videos]):
         door_close_time = start_frame.loc[v_index, 'door_close']
         start_time = start_frame.loc[v_index, 'n']
         five_min_frame_no = int(5 * 60 * 30)
-        offset = int(start_time - door_close_time)
+        offset = int(door_close_time - start_time)
 
-        epoch_1_label = label[:five_min_frame_no + offset]
+        epoch_1_label = label[offset:five_min_frame_no + offset]
         epoch_2_label = label[five_min_frame_no + offset: five_min_frame_no * 2 + offset]
         epoch_3_label = label[five_min_frame_no * 2 + offset: five_min_frame_no * 3 + offset]
 
@@ -164,7 +164,7 @@ for j, videos in enumerate([control_videos, BD_videos]):
         epoch_2_motif_usage = get_motif_usage(epoch_2_label, n_cluster)
         epoch_3_motif_usage = get_motif_usage(epoch_3_label, n_cluster)
 
-        epoch_1_latent_vector = latent_vector[:five_min_frame_no + offset]
+        epoch_1_latent_vector = latent_vector[offset: five_min_frame_no + offset]
         epoch_2_latent_vector = latent_vector[five_min_frame_no + offset: five_min_frame_no * 2 + offset]
         epoch_3_latent_vector = latent_vector[five_min_frame_no * 2 + offset: five_min_frame_no * 3 + offset]
 
@@ -208,8 +208,6 @@ for j, videos in enumerate([control_videos, BD_videos]):
         Epoch3_num_ones[j].append(num_one_item)
         Epoch3_num_zeros[j].append(num_zero_item)
 
-
-
         if i == 0:
             l = label
             latent = latent_vector
@@ -246,12 +244,13 @@ for j, videos in enumerate([control_videos, BD_videos]):
     TM[j] = tm/12
 #%%   Population-level plots
 #%% Plot L0 measures: spasity, entropy, number of 1s, number of 0s
-num_metrics = 6
+num_metrics = 4
 metric_names = ['distribution of entropy',
                 'distribution of #empty state',
                 'distribution of #p(state) = 1',
                 'distribution of #p(state) = 0',
                 'is_BD']
+lims = [[-2, 4], [-5, 15], [-4, 8], [30, 120]]
 fig, axes = plt.subplots(num_metrics, figsize=(5, 10))
 sns.set_style('darkgrid')
 CP_idx = np.zeros(12)
@@ -263,15 +262,19 @@ latent_ds = pd.DataFrame(np.concatenate((
     np.concatenate((num_zeros[0],num_zeros[1]),0).reshape(-1, 1),
     np.concatenate((CP_idx, BD_idx),0).reshape(-1, 1)), 1),
     columns=metric_names)
-
+sns.set_style('darkgrid')
 for i in range(num_metrics):
     sns.violinplot(x=metric_names[-1], y=metric_names[i],
                 data=latent_ds, palette="muted", ax=axes[i])
     sns.stripplot(y=metric_names[i], x=metric_names[-1], data=latent_ds,
                   color="white", edgecolor="gray", ax=axes[i])
+    CP = np.asarray(latent_ds[metric_names[i]][:12])
+    BD = np.asarray(latent_ds[metric_names[i]][12:])
+    s = stats.ttest_ind(CP, BD, nan_policy='omit', equal_var=False)
+    print("{} t-stat: {:.2f}, p-val: {:.3f}".format(metric_names[i], s.statistic, s.pvalue))
     axes[i].set_xticklabels(['CP','BD'])
-    axes[i].set_title('{}'.format(metric_names[i]))
-    axes[i].set_ylim([-1, 3])
+    axes[i].set_title('{}, p-val: {:.3f}'.format(metric_names[i], s.pvalue))
+    axes[i].set_ylim(lims[i])
     axes[i].set_xlabel('population')
 
 plt.suptitle("15-min")
@@ -282,11 +285,11 @@ def similarity_func(u, v):
     return 1/(1+np.linalg.norm(np.asarray(u)-np.asarray(v)))
 
 l2_matrix = np.zeros((24,24))
-l1_matrix = np.zeros((24,24))
+# l1_matrix = np.zeros((24,24))
 for i in range(24):
     for j in range(24):
         l2_matrix[i][j] = np.linalg.norm(transition_matrices[i]-transition_matrices[j])
-        l1_matrix[i][j] = cityblock(transition_matrices[i],transition_matrices[j])
+        # l1_matrix[i][j] = cityblock(transition_matrices[i],transition_matrices[j])
 
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 im = ax.imshow(l2_matrix)
@@ -297,16 +300,16 @@ ax.set_yticks(np.arange(24), patient_names, rotation=45)
 plt.colorbar(im)
 fig.show()
 
-fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-im = ax.imshow(l1_matrix)
-ax.set_title('15-min L1 distance of transition matrix')
-patient_names = control_videos + BD_videos
-ax.set_xticks(np.arange(24), patient_names, rotation=45)
-ax.set_yticks(np.arange(24), patient_names, rotation=45)
-plt.colorbar(im)
-fig.show()
+# fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+# im = ax.imshow(l1_matrix)
+# ax.set_title('15-min L1 distance of transition matrix')
+# patient_names = control_videos + BD_videos
+# ax.set_xticks(np.arange(24), patient_names, rotation=45)
+# ax.set_yticks(np.arange(24), patient_names, rotation=45)
+# plt.colorbar(im)
+# fig.show()
 #%% inspect high similarity ones
-
+patient_names = control_videos + BD_videos
 idx = [[11, 4], [17, 12], [16, 13]]
 for i in idx:
     idx_0 = i[0]
@@ -326,65 +329,52 @@ for i in idx:
     fig.show()
 #%% Epoch level plots
 
-
 #%% violin plot
+num_metrics = 4
+metric_names = ['distribution of entropy',
+                'distribution of #empty state',
+                'distribution of #p(state) = 1',
+                'distribution of #p(state) = 0',
+                'is_BD']
+lims = [[-2, 4], [-5, 15], [-4, 8], [30, 120]]
+CP_idx = np.zeros(12)
+BD_idx = np.ones(12)
 for epoch in range(1,4):
     entropy = eval("Epoch{}_Entropies".format(epoch))
     num_zero_rows = eval("Epoch{}_num_zero_rows".format(epoch))
     num_ones = eval("Epoch{}_num_ones".format(epoch))
     num_zeros = eval("Epoch{}_num_zeros".format(epoch))
     print("Epoch {}".format(epoch))
-    fig, axes = plt.subplots(4, figsize=(5, 10))
 
+    fig, axes = plt.subplots(num_metrics, figsize=(5, 10))
     sns.set_style('darkgrid')
-    CP_idx = np.zeros(12)
-    BD_idx = np.ones(12)
+
     latent_ds = pd.DataFrame(np.concatenate((
         np.concatenate((entropy[0], entropy[1]), 0).reshape(-1, 1),
         np.concatenate((num_zero_rows[0], num_zero_rows[1]), 0).reshape(-1, 1),
         np.concatenate((num_ones[0], num_ones[1]), 0).reshape(-1, 1),
         np.concatenate((num_zeros[0], num_zeros[1]), 0).reshape(-1, 1),
         np.concatenate((CP_idx, BD_idx), 0).reshape(-1, 1)), 1),
-        columns=['entropy', 'distribution of #empty state', 'distribution of #p(state) = 1',
-                 'distribution of #p(state) = 0', 'is_BD'])
+        columns=metric_names)
 
-    sns.violinplot(y="entropy", x='is_BD',
-                   data=latent_ds, palette="muted", ax=axes[0])
-    sns.stripplot(y="entropy", x="is_BD", data=latent_ds,
-                  color="white", edgecolor="gray", ax=axes[0])
-    axes[0].set_xticklabels(['CP', 'BD'])
-    axes[0].set_xlabel('population')
-    axes[0].set_title('distribution of entropy')
-    axes[0].set_ylim([-1, 3])
-    sns.violinplot(x='is_BD', y="distribution of #empty state",
-                   data=latent_ds, palette="muted", ax=axes[1])
-    sns.stripplot(y="distribution of #empty state", x="is_BD", data=latent_ds,
-                  color="white", edgecolor="gray", ax=axes[1])
-    axes[1].set_xticklabels(['CP', 'BD'])
-    axes[1].set_title('distribution of #empty state')
-    axes[1].set_ylim([-5, 15])
-    axes[1].set_xlabel('population')
-    sns.violinplot(x='is_BD', y="distribution of #p(state) = 1",
-                   data=latent_ds, palette="muted", ax=axes[2])
-    sns.stripplot(y="distribution of #p(state) = 1", x="is_BD", data=latent_ds,
-                  color="white", edgecolor="gray", ax=axes[2])
-    axes[2].set_xticklabels(['CP', 'BD'])
-    axes[2].set_title('distribution of #p(state) = 1')
-    axes[2].set_ylim([-2, 8])
-    axes[2].set_xlabel('population')
-    sns.violinplot(x='is_BD', y="distribution of #p(state) = 0",
-                   data=latent_ds, palette="muted", ax=axes[3])
-    sns.stripplot(y="distribution of #p(state) = 0", x="is_BD", data=latent_ds,
-                  color="white", edgecolor="gray", ax=axes[3])
-    axes[3].set_xticklabels(['CP', 'BD'])
-    axes[3].set_title('distribution of #p(state) = 0')
-    axes[3].set_ylim([50, 120])
-    axes[3].set_xlabel('population')
+    for i in range(num_metrics):
+        sns.violinplot(x=metric_names[-1], y=metric_names[i],
+                       data=latent_ds, palette="muted", ax=axes[i])
+        sns.stripplot(y=metric_names[i], x=metric_names[-1], data=latent_ds,
+                      color="white", edgecolor="gray", ax=axes[i])
+        CP = np.asarray(latent_ds[metric_names[i]][:12])
+        BD = np.asarray(latent_ds[metric_names[i]][12:])
+        s = stats.ttest_ind(CP, BD, nan_policy='omit', equal_var=False)
+        print("{} , t-stat: {:.2f}, p-val: {:.3f}".format(metric_names[i], s.statistic, s.pvalue))
+        axes[i].set_xticklabels(['CP', 'BD'])
+        axes[i].set_title('{}, p-val: {:.3f}'.format(metric_names[i], s.pvalue))
+        axes[i].set_ylim(lims[i])
+        axes[i].set_xlabel('population')
     plt.suptitle("Epoch {}".format(epoch))
-    plt.show()
+    fig.show()
+
 #%%
 from scipy.spatial.distance import euclidean, pdist, squareform
-
 for epoch in range(1,4):
     sim_matrix = np.zeros((24,24))
     epoch_tm = eval('Epoch{}_transition_matrix'.format(epoch))
@@ -421,6 +411,51 @@ for epoch in range(1,4):
         pwd = r'D:\OneDrive - UC San Diego\GitHub\Behavior-VAE\BD20-Jun5-2022\figure\transition_matrices\epoch'
         fname = "{}_{}_transition_epoch{}.png".format(patient_names[i], n_cluster, epoch)
         fig.savefig(os.path.join(pwd, fname))
+
+
+#%% Sliding window of 5 min analysis
+
+slide_window = {
+  "start_frame": [],
+  "is_BD": [],
+  "entropy": [],
+  "num_zero_row":[],
+  "num_one_item": [],
+  "num_zero_item":[]
+}
+for j, videos in enumerate([control_videos, BD_videos]):
+    for i in range(len(videos)):
+        v = videos[i]
+        print("Loading {} data...".format(v))
+        folder = os.path.join(cfg['project_path'], "results", v, model_name, 'kmeans-' + str(n_cluster), "")
+        label = np.load(r'D:\OneDrive - UC San Diego\GitHub\hBPMskeleton\{}\results\{}\VAME\kmeans-{}\{}_km_label_{}.npy'.format(project_name, v,n_cluster,n_cluster,v))
+        latent_vector = np.load(os.path.join(folder, 'latent_vector_' + v + '.npy')) # L x 30
+
+        v_index = start_frame.loc[start_frame['video_name'] == v].index.values[0]
+        door_close_time = start_frame.loc[v_index, 'door_close']
+        start_time = start_frame.loc[v_index, 'n']
+        window_size = int(3 * 60 * 30)
+        offset = int(door_close_time - start_time)
+
+        for k in range(len(label) - window_size + 1):
+            if k % 1000 == 0:
+                print(" Sliding window {} - {}...".format(offset + k, window_size + offset + k))
+            window_label = label[offset + k: window_size + offset + k]
+            window_motif_usage = get_motif_usage(window_label, n_cluster)
+            window_latent_vector = latent_vector[offset + k: window_size + offset + k]
+            window_transition_matrix = compute_transition_matrices([v], [window_label], n_cluster)
+            num_zero_row, num_one_item, num_zero_item = count_zeros(window_transition_matrix[0])
+            entropy = compute_l0_entropy(window_transition_matrix[0], window_label[-1])
+
+            slide_window["start_frame"].append(k)
+            slide_window["is_BD"].append(j)
+            slide_window["entropy"].append(entropy)
+            slide_window["num_zero_row"].append(num_zero_row)
+            slide_window["num_one_item"].append(num_one_item)
+            slide_window["num_zero_item"].append(num_zero_item)
+
+
+
 
 
 
